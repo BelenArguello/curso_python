ffbeebe4445bfaee00661ef39a153e5d704b1c1f
Fatima <soporte2@cti.com.py>
2025-02-13 10:33:49 -0300

    leccion 1

---
+++ tipos_de_datos.py
@@ -0,0 +1,28 @@
+# String
+# nombre = "Alex"
+# apellido = "Smith"
+# nombreCompleto = "Hola " + nombre + " " + apellido
+# print ("Hola " + nombre)
+# type: comprueba el tipo de dato
+# print(type(nombre))
+# print(nombreCompleto)
+
+#Int
+# edad = 24
+#edad= edad + 1
+# edad += 1
+# print(type(edad))
+# print("Tu edad es: " + str(edad))
+
+# float
+# altura = 180.5
+# print(type(altura))
+# print("Tu altura es: " + str(altura))
+
+#Boolean
+humano = True
+# print(type(humano))
+print("Eres un humano: " + str(humano))
+
+
+
