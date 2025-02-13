65c79c9a7e8bb9ad568df46398918d31b1847ac9
Fatima <soporte2@cti.com.py>
2025-02-13 15:18:28 -0300

    Leccion 4 y 5

---
+++ convertir_tipo_de_datos.py
@@ -0,0 +1,15 @@
+x = 1
+y = 2.0
+z = "3"
+
+#Almacena el cambio en la variable
+y = str(y)
+x = str(x)
+
+
+print(x)
+print(y)
+print(z)
+
+edad = 24
+print("Edad: " + str(edad))
---
+++ ingresar_texto.py
@@ -0,0 +1,10 @@
+nombre = input("Cual es tu nombre? ")
+#Convertir el dato ingresado en enteroF
+edad = int(input("Cuantos años tienes? "))
+altura = float(input("Que tan alto eres? "))
+
+edad += 1
+
+print("Hola " + nombre)
+print("Tienes " + str(edad) + " años")
+print("Mides " + str(altura) + " cm")
