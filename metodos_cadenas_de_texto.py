862edbfd16464851d7bc41f6d03546a49cd292d4
Fatima <soporte2@cti.com.py>
2025-02-13 11:23:27 -0300

    leccion 3

---
+++ metodos_cadenas_de_texto.py
@@ -0,0 +1,31 @@
+nombre = "fatima code"
+
+#len: devuelve la longitud de una cadena
+print(len(nombre))
+
+#Metodo de busqueda
+print(nombre.find("F"))
+
+#Metodo Capitalize
+print(nombre.capitalize())
+
+#Metodo para convertir toda la cadena a mayuscula
+print(nombre.upper())
+
+#Metodo para convertir la cadena a minuscula
+print(nombre.lower())
+
+#Metodo para saber si la cadena es un digito
+print(nombre.isdigit())
+
+#Metodo para saber si la cadena tiene espacio o caracteres especiales
+print(nombre.isalpha())
+
+#Metodo para saber cuantas veces se repite un caracter dentro de un cadena, distingue mayusculas de minusculas
+print(nombre.count("a"))
+
+#Metodo para reemplazar caracteres y cadenas 
+print(nombre.replace("c", "a"))
+
+#Para repetir una cadena varias veces
+print(nombre*4)
