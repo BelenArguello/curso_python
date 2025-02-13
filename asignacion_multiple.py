f7c099d250d82d86d9e6e23cd2454ec86a180ac7
Fatima <soporte2@cti.com.py>
2025-02-13 10:47:31 -0300

    leccion 2

---
+++ asignacion_multiple.py
@@ -0,0 +1,23 @@
+# nombre = "Fatima"
+# edad = "22"
+# estudiante = True
+
+
+# nombre, edad, estudiante = "Fatima", "23", True
+# print(nombre)
+# print(edad)
+# print(estudiante)
+
+
+#Solamente cuando a todas las variables se les asigna el mismo valor se puede utilizar este tipo de asignacion
+#nombre1 = 10
+#nombre2 = 10
+#nombre3 = 10
+#nombre4 = 10
+
+nombre1 = nombre2 = nombre3 = nombre4 = 10
+
+print(nombre1)
+print(nombre2)
+print(nombre3)
+print(nombre4)
